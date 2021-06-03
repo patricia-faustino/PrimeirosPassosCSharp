0d2b33a1b379c4f558bb1b84b6a1ede7670a3e9d
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-06-03 20:58:50 -0300

    Trabalhando com boolean

---
+++ 8-Condicionais2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Condicionais2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 8 - Condicionais 2");
+            int idadeJoao = 16;
+            int quantidadePessoas = 2;
+
+            /* bool maiorDeIdade = idadeJoao >= 18;
+             bool acompanhado = quantidadePessoas >= 2;*/
+
+            bool acompanhado = true;
+
+            if (acompanhado)
+            {
+                Console.WriteLine("Pode entrar.");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar.");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
