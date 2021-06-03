bf51d8e3c127b55263987b4db82a58c8ab983965
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-06-03 20:36:45 -0300

    Entendendo atribuicao a variaveis

---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 6");
+
+            int idade = 24;
+            int idadePatricia = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadePatricia);
+
+            Console.ReadLine();
+        }
+    }
+}
