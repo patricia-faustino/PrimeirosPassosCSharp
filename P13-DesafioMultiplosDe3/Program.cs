834910493ab4ae035ca845f4a16c45d9548e8ba3
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-06-03 22:23:02 -0300

    Desafio multiplos de 3

---
+++ P13-DesafioMultiplosDe3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P14_DesafioMultiplosDe3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 14");
+
+            for(int i = 0; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                    Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
