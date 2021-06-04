fd7c13fa6754f75002354a507904667a147d5e86
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-06-03 22:25:24 -0300

    Desafio Fatorial

---
+++ P15-Fatorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P15_Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 15");
+
+            int fatorial = 1;
+            for (int i = 1; i < 11; i++)
+            {
+                fatorial *= i;
+                Console.WriteLine($"Fatorial de {i} = {fatorial}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
