44dabf449f3609fe8f46b17d061ed8ffcf14dc9c
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-06-03 21:38:18 -0300

    Entendendo estrutura de repeticao while e operador de incremento

---
+++ 10-CalculaPoupanca/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10 - Calcula poupança");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            while(contadorMes <= 12)
+            {
+
+                valorInvestido = valorInvestido + valorInvestido * (0.36 / 100);
+                Console.WriteLine($"Após {contadorMes} mês, você terá R$  {valorInvestido}");
+                contadorMes++;
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
